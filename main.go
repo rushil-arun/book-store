f570f8074f0f313afe6d2c931df53126ddedbfae
rushil-arun <rushilarun@gmail.com>
2026-01-20 23:16:12 -0800

    Adding entry points to book store API

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/rushil-arun/book-store/book"
+	"github.com/rushil-arun/book-store/handling"
+	"github.com/rushil-arun/book-store/responses"
+)
 
 func main() {
-	fmt.Println("Hi!")
+	store := book.NewBookStore()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		responses.ResponseJSON(w, http.StatusOK, map[string]string{
+			"status": "healthy",
+			"time":   time.Now().Format(time.RFC3339),
+		})
+	})
+
+	r.Route("/api", func(rc chi.Router) {
+		bookHandler := handling.NewBookHandler(store)
+		r.Mount("/books", bookHandler.Routes())
+	})
+
+	addr := "localhost:3000"
+	log.Printf("Server on: %s", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
