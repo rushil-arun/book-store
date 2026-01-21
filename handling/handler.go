24ca6a3f72687e99e53cceb4f13873c7122def75
rushil-arun <rushilarun@gmail.com>
2026-01-20 23:08:45 -0800

    Handling routes for Handler

--- handling/handler.go
+++ handling/handler.go
@@ -1,6 +1,7 @@
 package handling
 
 import (
+	"github.com/go-chi/chi"
 	"github.com/rushil-arun/book-store/book"
 )
 
@@ -12,4 +13,19 @@ func NewBookHandler(store *book.BookStore) *BookHandler {
 	return &BookHandler{
 		store: store,
 	}
+}
+
+func (h *BookHandler) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Get("/", h.List)
+	r.Post("/", h.Create)
+
+	r.Route("/{bookID}", func(rc chi.Router) {
+		rc.Get("/", h.Get)
+		rc.Put("/", h.Update)
+		rc.Delete("/", h.Delete)
+	})
+
+	return r
 }
