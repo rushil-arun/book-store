bdc36d163bd417533cfccad9d32d9551e3d2159a
rushil-arun <rushilarun@gmail.com>
2026-01-20 22:34:49 -0800

    Fixing Handler.Get to search based on URL Param (id)

--- handling/routes.go
+++ handling/routes.go
@@ -3,13 +3,18 @@ package handling
 import (
 	"net/http"
 
-	"github.com/rushil-arun/book-store/book"
+	"github.com/go-chi/chi"
 	"github.com/rushil-arun/book-store/responses"
 )
 
 func (h *BookHandler) Get(w http.ResponseWriter, r *http.Request) {
-	book := r.Context().Value("book").(book.Book)
-	responses.ResponseJSON(w, http.StatusOK, book)
+	id := chi.URLParam(r, "bookID")
+	book, ok := h.store.Get(id)
+	if ok {
+		responses.ResponseJSON(w, http.StatusOK, book)
+	} else {
+		responses.ResponseError(w, http.StatusBadRequest, "Book with specified ID doesn't exist.")
+	}
 }
 
 func (h *BookHandler) List(w http.ResponseWriter, r *http.Request) {
